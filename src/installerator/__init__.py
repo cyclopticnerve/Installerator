ea8838b3989bb90394f5630edbc2a5813c8cf04e
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2022-11-21 10:24:42 -0500

    added modules to package imports for dot notation

--- src/installerator/__init__.py
+++ src/installerator/__init__.py
@@ -12,4 +12,8 @@ __version__ = "0.1.0"
 A small Python module that makes installing Python apps easier
 '''
 
+# import all modules in package to get full dot notation from package import
+from installerator import installerator     # noqa W0611 (unused import)
+from installerator import uninstallerator   # noqa W0611 (unused import)
+
 # -)
