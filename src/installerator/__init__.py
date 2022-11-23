b4759f47efbf3d1b6d4897ba418f9035894bec1a
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2022-11-23 12:15:24 -0500

    added __all__, changed quotes

--- src/installerator/__init__.py
+++ src/installerator/__init__.py
@@ -8,12 +8,15 @@
 
 __version__ = "0.1.0"
 
-'''
+"""
 A small Python module that makes installing Python apps easier
-'''
+"""
 
 # import all modules in package to get full dot notation from package import
 import installerator     # noqa W0611 (unused import)
 import uninstallerator   # noqa W0611 (unused import)
+
+# from <package_name> import *
+__all__ = ["installerator", "uninstallerator"]
 
 # -)
